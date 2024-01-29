eb70024c22e54f3266227b2189aebda13a9226e0
pink <haibarapink@gmail.com>
2024-01-29 09:16:43 +0800

    Limit算子实现完成

--- godb/limit_op.go
+++ godb/limit_op.go
@@ -9,21 +9,33 @@ type LimitOp struct {
 // Limit constructor -- should save how many tuples to return and the child op.
 // lim is how many tuples to return and child is the child op.
 func NewLimitOp(lim Expr, child Operator) *LimitOp {
-	// TODO: some code goes here
-	return nil
+	var l LimitOp
+	l.child = child
+	l.limitTups = lim
+	return &l
 }
 
 // Return a TupleDescriptor for this limit
 func (l *LimitOp) Descriptor() *TupleDesc {
-	// TODO: some code goes here
-	return nil
-
+	return l.child.Descriptor()
 }
 
 // Limit operator implementation. This function should iterate over the
 // results of the child iterator, and limit the result set to the first
 // [lim] tuples it sees (where lim is specified in the constructor).
 func (l *LimitOp) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
-	// TODO: some code goes here
-	return nil, nil
+	cnt := 0
+	lim := l.limitTups.(*ConstExpr).val.(IntField).Value
+	iter, err := l.child.Iterator(tid)
+	if err != nil {
+		return nil, err
+	}
+
+	return func() (*Tuple, error) {
+		if int64(cnt) == lim {
+			return nil, nil
+		}
+		cnt++
+		return iter()
+	}, nil
 }
